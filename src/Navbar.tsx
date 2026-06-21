0e463ca465e1c8ef54cc8a4aa0fb49536070fb2c
Yassir Dahraoui <dahraoui.yassir@educ.di.unito.it>
2026-06-21 23:11:27 +0200

    quick fixes for navbar responsiveness

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -28,33 +28,44 @@ export function Navbar(setter: pageProps) {
                               onClick={() => setter.setPage("loginR")}>Login</button>;
     else
         mainContent = <button className="button py-1 is-light is-outlined"
-                    onClick={ handleLogout }>Logout</button>;
+                              onClick={ handleLogout }>Logout</button>;
 
     return (
-        <div className="navbar"><div className="is-flex is-align-items-center m-2" style={{ gap: "10px" }}>
-            <h1 className="title m-0" onClick={() => setter.setPage("home")}>LifeChoosesU
-            </h1>{user && (<span className="tag is-rounded is-italic is-white">
-            {user.role === "MEDIATORE" ? "MEDIATOR AREA" : "HELP AREA"}</span>)}
-        </div>
+        <div className="navbar">
+            <div className="is-flex is-align-items-center m-2" style={{ gap: "10px" }}>
+                <h1 className="title m-0 has-text-white" onClick={() => setter.setPage("home")}>
+                    LifeChoosesU
+                </h1>
+                {user && (
+                    <span className="tag is-rounded is-italic is-white">
+                        {user.role === "MEDIATORE" ? "MEDIATOR AREA" : "HELP AREA"}
+                    </span>
+                )}
+            </div>
+
             <div className="nav-buttons">
                 { setter.page !== "home" ?
-                <button className="button is-warning mx-4"
-                        onClick={() => setter.setPage("home")}>Home</button> : null }
+                    <button className="button is-warning"
+                            onClick={() => setter.setPage("home")}>Home</button> : null }
+
                 {(user !== null && user.role === "RICHIEDENTE") && setter.page !== "support" ?
-                    <button className="button is-success is-light mx-4"
+                    <button className="button is-success is-light"
                             onClick={() => setter.setPage("support")}>+ Nuova richiesta</button> : null}
+
                 {(user !== null && user.role === "RICHIEDENTE") && setter.page !== "requests" ?
-                    <button className="button is-primary is-dark mx-4"
+                    <button className="button is-primary is-dark"
                             onClick={() => setter.setPage("requests")}>Le mie richieste</button> : null}
+
                 {(user !== null && user.role === "MEDIATORE") && setter.page !== "dashboard" && setter.page !== "dashboard*" ?
-                    <button className="button is-black mx-4"
+                    <button className="button is-black"
                             onClick={() => setter.setPage("dashboard")}>La mia dashboard</button> : null}
+
                 { (setter.page !== "loginM" && setter.page !=="loginR"
                     && setter.page !== "registerM" && setter.page !== "registerR") && (
-                    <div className="is-flex is-flex-direction-column is-align-items-center mx-0">
+                    <div className="is-flex is-flex-direction-column is-align-items-center">
                         {user && (
                             <div className="box has-background-light px-2 py-0 mb-1 is-rounded is-size-7 is-flex is-align-items-center">
-                                 <img className="image is-16x16 mr-2" src="/user.png" alt=""/>
+                                <img className="image is-16x16 mr-2" src="/user.png" alt=""/>
                                 <strong className="has-text-black">{user.username}</strong>
                             </div>
                         )}
